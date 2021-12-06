dcf916e3595f167ae733f9192756d03e7caafae2
Zachary_Anderson <zanderson3j@gmail.com>
2021-12-06 09:34:07 -0500

    day 6

--- src/advent_of_code/day_06.clj
+++ src/advent_of_code/day_06.clj
@@ -1,11 +1,79 @@
 (ns advent-of-code.day-06)
 
+(defn- readNumbers [data]
+  (->> data
+       (re-seq #"-?\d+")
+       (map #(Integer/parseInt %))))
+
+(defn- makeZeroedArray [numberOfElements]
+  (loop [list []
+         element 0]
+    (if (= numberOfElements element)
+      list
+      (recur (conj list 0) (+ element 1)))))
+
+;(defn- spawn [lanternFish]
+;  (loop [numberToSpawn (count (filter #(= % -1) lanternFish))
+;         populatedSchool lanternFish]
+;    (if (= numberToSpawn 0)
+;      populatedSchool
+;      (recur (dec numberToSpawn) (conj populatedSchool 8)))))
+
+;(defn- passTheDay [lanternFish]
+;  (->> lanternFish
+;       (map #(dec %))
+;       (spawn)
+;       (map #(if (neg? %) 6 %))))
+
+;(defn- runLanternFishSimulation [startingFish days]
+;  (loop [daysLeft days
+;         lanternFish (list startingFish)]
+;    (println daysLeft)
+;    (if (= daysLeft 0)
+;      lanternFish
+;      (recur (dec daysLeft) (passTheDay lanternFish)))))
+
+(defn- spawnParentsOffspring [counts startingDay]
+  (loop [index startingDay
+         countsOfOffspring counts]
+    (if (> index (dec (count counts)))
+      countsOfOffspring
+      (recur (+ index 7) (update countsOfOffspring index inc)))))
+
+(defn- spawnOffspringsOffspring [counts startingDay]
+  (loop [index startingDay
+         numberSpawning (nth counts (- startingDay 9))
+         countsOfOffspring counts]
+    (if (> index (dec (count counts)))
+      countsOfOffspring
+      (recur (+ index 7) numberSpawning (update countsOfOffspring index + numberSpawning)))))
+
+(defn- getOffspring [startingFish days]
+  (loop [day 9
+         counts (as-> days $
+                      (inc $)
+                      (makeZeroedArray $)
+                      (spawnParentsOffspring $ (inc startingFish)))]
+    (if (> day (dec (count counts)))
+      counts
+      (recur (inc day) (spawnOffspringsOffspring counts day)))))
+
 (defn part-1
   "Day 06 Part 1"
   [input]
-  input)
+  (as-> input $
+        (readNumbers $)
+        (map #(getOffspring % 80) $)
+        (+ (count $) (reduce + (flatten $)))))
+
+;(map #(runLanternFishSimulation % 80) $)
+;(flatten $)
+;(count $)
 
 (defn part-2
   "Day 06 Part 2"
   [input]
-  input)
+  (as-> input $
+        (readNumbers $)
+        (map #(getOffspring % 256) $)
+        (+ (count $) (reduce + (flatten $)))))
--- test/advent_of_code/day_06_test.clj
+++ test/advent_of_code/day_06_test.clj
@@ -4,9 +4,9 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
+  (let [expected 5934]
     (is (= expected (part-1 (slurp (resource "day-06-example.txt")))))))
 
 (deftest part2
-  (let [expected nil]
+  (let [expected 26984457539]
     (is (= expected (part-2 (slurp (resource "day-06-example.txt")))))))
