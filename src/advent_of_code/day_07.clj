240f030489b6ced13f256d14451c7f788bd6161c
Zachary_Anderson <zanderson3j@gmail.com>
2021-12-07 00:31:08 -0500

    day 7

--- src/advent_of_code/day_07.clj
+++ src/advent_of_code/day_07.clj
@@ -1,11 +1,48 @@
 (ns advent-of-code.day-07)
 
+(defn- readNumbers [data]
+  (->> data
+       (re-seq #"-?\d+")
+       (map #(Integer/parseInt %))))
+
+(defn- incrementedFuelMove [finalPosition startingPosition]
+  (let [numberOfSteps (Math/abs (int (- startingPosition finalPosition)))]
+    (loop [step 1
+           sum 0]
+      (if (> step numberOfSteps)
+        sum
+        (recur (+ step 1) (+ sum step))))))
+
+(defn- fuelUsageIncremented [positions maxPos]
+  (loop [position maxPos
+         fuelCounts []]
+    (if (< position 0)
+      fuelCounts
+      (recur (dec position) (conj fuelCounts (->> positions
+                                                  (map #(incrementedFuelMove position %))
+                                                  (reduce +)))))))
+
+(defn- fuelUsage [positions maxPos]
+  (loop [position maxPos
+         fuelCounts []]
+    (if (< position 0)
+      fuelCounts
+      (recur (dec position) (conj fuelCounts (->> positions
+                                                  (map #(Math/abs (int (- position %))))
+                                                  (reduce +)))))))
+
 (defn part-1
   "Day 07 Part 1"
   [input]
-  input)
+  (as-> input $
+        (readNumbers $)
+        (fuelUsage $ (reduce max $))
+        (reduce min $)))
 
 (defn part-2
   "Day 07 Part 2"
   [input]
-  input)
+  (as-> input $
+        (readNumbers $)
+        (fuelUsageIncremented $ (reduce max $))
+        (reduce min $)))
